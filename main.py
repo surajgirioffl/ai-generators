dbc45b9766a824acda5d46c1cc9f3f878eb96a6e
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-09 17:01:48 +0530

    Add: Add driver module to integrate & execute the bot
    Add driver module to integrate and execute the bot.
    
    Signed-off-by: @surajgirioffl

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Driver module to integrate and execute the script.
+
+Driver module to integrate and execute the script.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 7th May 2024
+Last-modified: 9th May 2024
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
+__version__ = "0.0.0"
