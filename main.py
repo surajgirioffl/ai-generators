3780a73ac18277f89d149aabebfcc822bc55f00d
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-09 17:03:33 +0530

    Feat: Add the logging configuration for the bot
    Add the logging configuration for the bot.
    
    Signed-off-by: @surajgirioffl

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ Last-modified: 9th May 2024
 __author__ = "Suraj Kumar Giri"
 __email__ = "surajgirioffl@gmail.com"
 __version__ = "0.0.0"
+
+
+import logging
+import os
+import re
+from datetime import datetime
+
+
+def configure_logging(filename: str = "appdata/script.log") -> None:
+    """Configure logging with a specified filename or default 'appdata/script.log'.
+
+    Args:
+        filename (str): The name of the file to log to. Defaults to 'appdata/script.log'.
+
+    Returns:
+        None
+    """
+    logging.basicConfig(filename=filename, level=logging.INFO, format="%(asctime)s - %(module)s(%(lineno)d) - %(levelname)s -> %(message)s")
