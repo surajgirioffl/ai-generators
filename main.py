d3623601ff3dda3dd8a4dfee44cc0888f1808912
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-11 09:45:40 +0530

    Update: Update the module docstring
    Update the module docstring and little more changes.
    
    Signed-off-by: @surjagirioffl

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 Driver module to integrate and execute the script.
 Author: Suraj Kumar Giri (@surajgirioffl)
 Init-date: 7th May 2024
-Last-modified: 9th May 2024
+Last-modified: 11th May 2024
+Error-series: 1100
 """
 
 __author__ = "Suraj Kumar Giri"
@@ -14,7 +15,6 @@ __version__ = "0.0.0"
 import logging
 import os
 import re
-from datetime import datetime
 
 
 def configure_logging(filename: str = "appdata/script.log") -> None:
