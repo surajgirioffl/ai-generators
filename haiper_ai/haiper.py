7e1f14f0160b0dd2f5781c6d4c98a8ed1ae80069
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-17 16:13:12 +0530

    Add: Add module to handle all the operations related to the haiper
    Add module to handle all the operations related to the haiper.
    
    Signed-off-by: @surajgirioffl

---
+++ haiper_ai/haiper.py
@@ -0,0 +1,7 @@
+"""Module to handle all the operations related to haiper.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 17th May 2024
+Last-modified: 17th May 2024
+Error-series: 1400
+"""
