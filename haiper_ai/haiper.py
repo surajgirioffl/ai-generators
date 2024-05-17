8e622fbe0941057e6b24c496f4d284b9c2f00a2b
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-18 00:01:17 +0530

    Feat: Add feat. to create video with image
    Add functionality to create video with image.
    
    Signed-off-by: @surajgirioffl

--- haiper_ai/haiper.py
+++ haiper_ai/haiper.py
@@ -83,3 +83,41 @@ class Haiper:
 
         create_button_xpath = "/html/body/main/article/section/div/div/footer/div/form/div/div[3]/button[3]"
         self.wait.until(EC.element_to_be_clickable((By.XPATH, create_button_xpath))).click()  # Clicking create button
+
+    def create_video_with_image(self, image_path: str, seed: str | int, prompt: str = "", duration: str | int = 2):
+        """A function to create a video with an image.
+
+        Args:
+            image_path (str): The path to the image file.
+            seed (str | int): The seed for the video creation.
+            prompt (str, optional): The prompt to be included. Defaults to ""(empty).
+            duration (str | int, optional): The duration of the video. Defaults to 2.
+        """
+
+        def wait_until_image_uploaded():
+            """Wait until the image is uploaded by waiting for the presence of the specified CSS selector."""
+            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='thumbnail']")))
+
+        animate_your_image_div_xpath = "/html/body/main/article/section/div/div/div[2]/div[1]/div/div/div/div[2]"
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, animate_your_image_div_xpath))).click()
+
+        # Image upload
+        self.driver.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(image_path)
+        if prompt:
+            self.driver.find_element(By.TAG_NAME, "textarea").send_keys(prompt)  # Prompt
+
+        self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Creation Setting"]').click()  # Clicking option button
+        self.wait.until(EC.visibility_of_element_located((By.ID, ":r5r:"))).send_keys(seed)
+
+        duration_2_sec_button_id = ":r5t:"  # default value in haiper
+        duration_4_sec_button_id = ":r5v:__label"
+        if str(duration) == "4":
+            self.driver.find_element(value=duration_4_sec_button_id).click()
+        else:
+            self.driver.find_element(value=duration_2_sec_button_id).click()
+
+        # Waiting until the image uploaded and the submit-button will clickable.
+        wait_until_image_uploaded()
+
+        create_button_xpath = "/html/body/main/article/section/div/div/footer/div/form/div/div[3]/button[3]"
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, create_button_xpath))).click()  # Clicking create button
