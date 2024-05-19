27f3752a9b182a7be41ba06e69c04c18c8453df3
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-20 04:02:19 +0530

    Add: Add driver module to integrate and exectue script
    Add the driver module to integrate and execute the script.
    
    Signed-off-by: @surajgirioffl

---
+++ ideogram_ai/main.py
@@ -0,0 +1,12 @@
+"""Driver module to integrate and execute the script.
+
+Driver module to integrate and execute the script.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 20th May 2024
+Last-modified: 20th May 2024
+Error-series: 1500
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
+__version__ = "0.0.0"
