e501514c53e9967d037ef23e069e78b8f3575fcb
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-20 02:02:14 +0530

    Add: Add module to handle all the operations of ideogram
    Add module to handle all the operations related to the ideogram.
    
    Signed-off-by: @surajgirioffl

---
+++ ideogram_ai/ideogram.py
@@ -0,0 +1,7 @@
+"""Module to handle all the operations related to ideogram.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 20th May 2024
+Last-modified: 20th May 2024
+Error-series: 1600
+"""
