9cb5c8da57c982acc79dc547997e199e79636e80
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-20 02:05:32 +0530

    Add: Add Ideogram class with its constructor
    Add Ideogram class with its constructor to handle all operations
    related to the Ideogram.
    
    Signed-off-by: @surajgirioffl

--- ideogram_ai/ideogram.py
+++ ideogram_ai/ideogram.py
@@ -5,3 +5,31 @@ Init-date: 20th May 2024
 Last-modified: 20th May 2024
 Error-series: 1600
 """
+
+import logging
+import os
+from time import sleep
+from typing import Any, Literal
+from datetime import datetime
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome, Edge
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+URL = "https://ideogram.ai/"
+
+
+class Ideogram:
+    """Class to handle all operations related to the Ideogram."""
+
+    def __init__(self, driver: Chrome | Edge | Any):
+        """Constructor of Ideogram class.
+        Initializes the class with the given driver object and sets up a WebDriverWait object.
+
+        Args:
+            driver (Chrome | Edge | Any): The driver object to be used for the class.
+        """
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 60)
