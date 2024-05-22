8ead51e9cdde99c4361cefb4b6fc2f6293895c9d
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-22 22:07:28 +0530

    Add: Add module to handle operations related to LensGo
    Add module to handle all the operations related to the LensGo.
    
    Signed-off-by: @surajgirioffl

---
+++ lensgo_ai/lensgo.py
@@ -0,0 +1,7 @@
+"""Module to handle all the operations related to LensGo.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 22nd May 2024
+Last-modified: 22nd May 2024
+Error-series: 1800
+"""
