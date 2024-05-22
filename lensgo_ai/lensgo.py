9492dbd11c8bce82774c6c6e14af2acfc4ffce55
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-22 22:10:41 +0530

    Feat: Add feat. to login to LensGo using Google authentication
    Add functionality to login to LensGo using Google authentication.
    
    Signed-off-by: @surajgirioffl

--- lensgo_ai/lensgo.py
+++ lensgo_ai/lensgo.py
@@ -33,3 +33,43 @@ class LensGo:
         """
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 60)
+
+    def login_with_google(self) -> bool:
+        """Function to login to LensGo using Google authentication.
+
+        Args:
+            driver (Chrome | Edge | Any): The driver to interact with the browser.
+
+        Returns:
+            bool: True if login is successful, False otherwise.
+        """
+        logging.info("Login LensGo via Google authentication.")
+        self.driver.get(URL)
+
+        try:
+            # Login with google appears by clicking log-in button or any other button performing any operation
+            # Clicking on Log-in button. So, Login with Google pop-up div will appear.
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".no-login"))).click()
+
+            # We can login via Google or using email & password.
+            # Log-in using Google.
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".google-btn"))).click()
+
+            # Account selection is compulsory in Lensgo
+            logging.info("Selecting Google account.")
+            self.wait.until(EC.url_contains("oauthchooseaccount"))  # Wait until account selection page opens
+            select_account_div_xpath = (
+                '//*[@id="yDmH0d"]/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[1]/form/span/section/div/div/div/div/ul/li[1]/div'
+            )
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, select_account_div_xpath))).click()
+
+            # Wait until login success
+            self.wait.until(EC.url_contains("lensgo"))
+        except Exception as e:
+            print("Login failed. Error Code: 1801")
+            logging.error("Login failed. Error Code: 1801")
+            logging.exception(f"Exception: {e}")
+            return False
+        else:
+            logging.info("Login success.")
+            return True
