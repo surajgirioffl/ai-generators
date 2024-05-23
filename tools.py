dc729aa915a4c2d608c8807912ecc2df3f23da3a
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-23 23:20:02 +0530

    Add: Add module containing various tools
    Add module containing various tools required for the script.
    
    Signed-off-by: @surajgirioffl

---
+++ tools.py
@@ -0,0 +1,7 @@
+"""Module containing various tools required for the script.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 23rd May 2024
+Last-modified: 23rd May 2024
+Error-series: 1300
+"""
