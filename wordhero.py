83202cc579770c781d427ee6f3e23251c898d802
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-23 22:45:04 +0530

    Add: Add module to handle all operations of WordHero
    Add module to handle all operations related to the WordHero.
    
    Signed-off-by: @surajgirioffl

---
+++ wordhero.py
@@ -0,0 +1,7 @@
+"""Module to handle all operations related to the WordHero.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 23rd May 2024
+Last-modified: 23rd May 2024
+Error-series: 1200
+"""
