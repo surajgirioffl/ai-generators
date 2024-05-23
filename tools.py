eadf42be8c92569c0c3d066a894b4d395ef5cb5e
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-24 00:13:34 +0530

    Feat: Add feat. to configure logging
    Add functionality to configure logging.
    
    Signed-off-by: @surajgirioffl

--- tools.py
+++ tools.py
@@ -5,3 +5,40 @@ Init-date: 23rd May 2024
 Last-modified: 23rd May 2024
 Error-series: 1300
 """
+
+import json
+import logging
+import os
+
+
+def load_settings(path: str = "settings.json") -> dict:
+    """Load settings from a JSON file.
+
+    Args:
+        path (str): The path to the JSON file. Defaults to "settings.json".
+
+    Returns:
+        dict: The settings loaded from the file.
+
+    Raises:
+        Same exception as encountered during loading.
+    """
+    try:
+        with open(path) as file:
+            return json.load(file)
+    except Exception as e:
+        logging.error("Error in loading settings file. Error Code: 1301")
+        logging.exception(f"Exception: {e}")
+        raise e.__class__(f"{e}")
+
+
+def configure_logging(filename: str = "appdata/script.log") -> None:
+    """Configure logging with a specified filename or default 'appdata/script.log'.
+
+    Args:
+        filename (str): The name of the file to log to. Defaults to 'appdata/script.log'.
+
+    Returns:
+        None
+    """
+    logging.basicConfig(filename=filename, level=logging.INFO, format="%(asctime)s - %(module)s(%(lineno)d) - %(levelname)s -> %(message)s")
