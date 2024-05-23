04ca61aa456237ea463786e929229a835ef0439b
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-24 00:22:11 +0530

    Feat: Add feat. to get webdriver instance
    Add funtionality to get the webdriver instance for the given browser.
    
    Signed-off-by: @surajgirioffl

--- tools.py
+++ tools.py
@@ -9,6 +9,7 @@ Error-series: 1300
 import json
 import logging
 import os
+from selenium.webdriver import Chrome, Edge, ChromeOptions, EdgeOptions
 
 
 def load_settings(path: str = "settings.json") -> dict:
@@ -55,3 +56,33 @@ def create_app_require_directories(dirs: list | tuple) -> None:
     """
     for directory in dirs:
         os.makedirs(directory, exist_ok=True)
+
+
+def get_webdriver_instance(browser: str = "chrome", headless=False, profile_dir_path: str | None = None) -> Chrome | Edge | None:
+    """Function to get the webdriver instance for the given browser.
+
+    Args:
+        browser (str, optional): Desired browser (chrome or edge). Defaults to "chrome".
+        headless (bool, optional): Set to True for headless mode. Defaults to False.
+        profile_dir_path (str, optional): Path to the profile directory. Defaults to None.
+
+    Returns:
+        Chrome | Edge | None: Webdriver instance if browser is supported, else None.
+    """
+    if browser == "chrome":
+        options = ChromeOptions()
+        if headless:
+            options.add_argument("--headless")
+        if profile_dir_path:
+            options.add_argument(f"--user-data-dir={profile_dir_path}")
+        return Chrome(options=options)
+    elif browser == "edge":
+        options = EdgeOptions()
+        if headless:
+            options.add_argument("--headless")
+        if profile_dir_path:
+            options.add_argument(f"--user-data-dir={profile_dir_path}")
+        return Edge(options=options)
+    else:
+        logging.error("Browser not supported. Please use Chrome or Edge. Error Code: 1302")
+        return None
