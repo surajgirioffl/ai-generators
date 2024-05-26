842aa8e19f1cb4763280a2842f5b9c0b01ec3b83
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-27 01:12:04 +0530

    Feat: Add module to handle operations related to Firefly
    Add module to handle all operations related to the Adobe Firefly.
    
    Signed-off-by: @surajgirioffl

---
+++ firefly.py
@@ -0,0 +1,7 @@
+"""Module to handle all operations related to the Adobe Firefly.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 27th May 2024
+Last-modified: 27th May 2024
+Error-series: 1400
+"""
