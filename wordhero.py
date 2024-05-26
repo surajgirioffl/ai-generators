9c273574c3c7797093cd6531bea761a784586d2e
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-27 01:00:54 +0530

    Update: Update `generate_article` method to make it better
    Update 'generate_article' method to make it better by returning desired
    value/data to the calling functions.
    
    Signed-off-by: @surajgirioffl

--- wordhero.py
+++ wordhero.py
@@ -195,18 +195,19 @@ class WordHero:
 
         return prompt_response_dict
 
-    def generate_article(self, headline: str, tone: str, number_of_words: int):
+    def generate_article(self, headline: str, tone: str, number_of_words: int) -> tuple[str, dict]:
         engine = inflect.engine()  # for ordinal number (1st, 2nd, etc.)
         number_of_part = number_of_words // 500
+        article: str = ""
 
         for part in range(1, number_of_part + 1):
             if part == 1:
                 prompt = f"""Write a {number_of_words} word article about "{headline}". Write in {number_of_part} parts with approximate 500 words each. Start with the headline, multiple subheadings. Content for each subheading should be 120-150 words long. Write in a {tone} tone. Give me the first part."""
-                prompt_response_dict = self.generate_content_with_chat(prompt)
-                with open("article.txt", "w") as file:
-                    file.write(prompt_response_dict[prompt])
+                prompt_response_dict: dict = self.generate_content_with_chat(prompt)
             else:
                 prompt = f"Give me the {engine.ordinal(part)} part."
-                prompt_response_dict = self.generate_content_with_chat(prompt, False)
-                with open("article.txt", "a") as file:
-                    file.write(prompt_response_dict[prompt])
+                prompt_response_dict: dict = self.generate_content_with_chat(prompt, False)
+
+            article += prompt_response_dict[prompt]
+
+        return article, prompt_response_dict
