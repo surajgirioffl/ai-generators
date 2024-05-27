2368ceea3638f574371cb887597185b0a53d1114
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-27 22:39:25 +0530

    Add: Add class to handle all operations related to Pixlr
    Add class to handle all operations related to the Pixlr.
    
    Signed-off-by: @surajgirioffl

--- pixlr.py
+++ pixlr.py
@@ -5,3 +5,25 @@ Init-date: 27th May 2024
 Last-modified: 27th May 2024
 Error-series: 1500
 """
+
+from typing import Any
+from selenium.webdriver import Chrome, Edge
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class Pixlr:
+    """Class to handle all operations related to the Pixlr"""
+
+    URL = "https://pixlr.com/image-generator/"
+
+    def __init__(self, driver: Chrome | Edge | Any | None) -> None:
+        """Constructor of WordHero class.
+        Initializes the class with the given driver object and sets up a WebDriverWait object.
+
+        Args:
+            driver (Chrome | Edge | Any): The driver object to be used for the class.
+        """
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 60)
