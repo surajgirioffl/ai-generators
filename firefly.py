2146471b6af50f78eb2bd5e58e82cdda6865fe99
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-27 21:42:08 +0530

    Add: Add `Firefly` class to handle all operations
    Add 'Firefly' class to handle all operations related to the Adobe
    Firefly.
    
    Signed-off-by: @surajgirioffl

--- firefly.py
+++ firefly.py
@@ -5,3 +5,15 @@ Init-date: 27th May 2024
 Last-modified: 27th May 2024
 Error-series: 1400
 """
+
+import requests
+
+
+class Firefly:
+    def __init__(self) -> None:
+        self.URL = "https://firefly-api.adobe.io/v2/images/generate"
+        ...
+
+    def generate_image(self, prompt: str) -> str:
+        requests.get(self.URL)
+        ...
