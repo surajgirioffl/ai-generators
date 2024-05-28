cdaa2ff265fad0b3eaf04a3257e1eed1dd8a9e90
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-05-29 01:02:46 +0530

    Add: Add module to handle all operations wrt Firefly
    Add module to handle all operations related to the Adobe Firefly.
    
    Signed-off-by: @surajgirioffl

---
+++ firefly_ai/firefly.py
@@ -0,0 +1,19 @@
+"""Module to handle all operations related to the Adobe Firefly.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 27th May 2024
+Last-modified: 27th May 2024
+Error-series: 11 00
+"""
+
+import requests
+
+
+class Firefly:
+    def __init__(self) -> None:
+        self.URL = "https://firefly-api.adobe.io/v2/images/generate"
+        ...
+
+    def generate_image(self, prompt: str) -> str:
+        requests.get(self.URL)
+        ...
