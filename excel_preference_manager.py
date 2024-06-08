56c14dbc345c1d7372b7e74adbbb7a0c7654bce9
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-08 23:56:44 +0530

    Add: Add module to handle operation related to preferences
    Module to handle all operations related to the Preferences provided
    via the Excel file.
    
    Signed-off-by: @surajgirioffl

---
+++ excel_preference_manager.py
@@ -0,0 +1,13 @@
+"""Module to handle all operations related to the Preferences provided via the Excel file.
+
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 8th June 2024
+Last-modified: 8th June 2024
+Error-series: 2100
+"""
+
+import pandas as pd
+
+
+class PreferenceManager:
+    """Class to handle all operations related to the Preferences provided via the Excel file."""
