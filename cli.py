c6ee5f6fd65e623e3bfa96d04c66cabcbea4445a
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-09 00:55:45 +0530

    Add: Add module to provide CLI for the application
    Add module to provide Command Line Interface for the application.
    
    Signed-off-by: @surajgirioffl

---
+++ cli.py
@@ -0,0 +1,12 @@
+"""CLI module to provide Command Line Interface for the application.
+
+CLI module to provide Command Line Interface for the application.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 09th June 2024
+Last-modified: 09th June 2024
+Error-series: 2200
+"""
+
+
+def main():
+    pass
