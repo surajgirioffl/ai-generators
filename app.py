d2a5cea68bea8f1c70d5cd393d429536e3fd0fb8
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-09 00:39:39 +0530

    Add: Add master module to integrate and execute the script
    Add master/driver module to integrate and execute the script.
    
    Signed-off-by: @surajgirioffl

---
+++ app.py
@@ -0,0 +1,12 @@
+"""Driver module to integrate and execute the script (Master script - Master Module)
+
+Driver module to integrate and execute the script.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 09th June 2024
+Last-modified: 09th June 2024
+Error-series: 3100
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
+__version__ = "0.0.0"
