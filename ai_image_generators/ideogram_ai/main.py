0771fe2a35360560cac947afe733f64de62f80ca
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-09 02:05:57 +0530

    Fix: Fix `ImportError`
    Fix the 'ImportError`.
    
    Signed-off-by: @surajgirioffl

--- ai_image_generators/ideogram_ai/main.py
+++ ai_image_generators/ideogram_ai/main.py
@@ -23,7 +23,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
-from ideogram import Ideogram
+from .ideogram import Ideogram
 
 PROJECT_DIR = os.path.dirname(__file__)
 os.chdir(PROJECT_DIR)  # Changing the current working directory to the project directory.
