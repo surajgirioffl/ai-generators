383d6a31a17ac119b43188ec4daa988f07aed130
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-09 00:56:40 +0530

    Add: Add module to provide GUI for the application
    Add module to provide Graphical User Interface for the application.
    
    Signed-off-by: @surajgirioffl

---
+++ gui.py
@@ -0,0 +1,12 @@
+"""GUI module to provide Graphical User Interface for the application.
+
+GUI module to provide Graphical User Interface for the application.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 09th June 2024
+Last-modified: 09th June 2024
+Error-series: 2300
+"""
+
+
+def main():
+    pass
