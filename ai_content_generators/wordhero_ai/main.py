84c34e98bb15828072429d240c1a9ca0212448e1
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-10 00:44:53 +0530

    Implement: Implement feat. to saving generated article
    Implement functionality to saving generated article. Also, add some
    logging messages.
    
    Signed-off-by: @surajgirioffl

--- ai_content_generators/wordhero_ai/main.py
+++ ai_content_generators/wordhero_ai/main.py
@@ -12,6 +12,7 @@ __email__ = "surajgirioffl@gmail.com"
 __version__ = "0.0.0"
 
 import os
+import logging
 
 if __name__ == "__main__":
     import tools
@@ -22,14 +23,11 @@ else:
     from .wordhero import WordHero
 
 
-APP_REQUIRED_DIRS = ["appdata", "appdata/logs", "appdata/profile"]
 PROJECT_DIR = os.path.dirname(__file__)
-
 os.chdir(PROJECT_DIR)  # Changing the current working directory to the project directory.
-
 SETTINGS: dict = tools.load_settings("config.json")
 
-
+APP_REQUIRED_DIRS = ["appdata", "appdata/logs", "appdata/profile", SETTINGS["output_location"]]
 tools.create_app_require_directories(APP_REQUIRED_DIRS)
 tools.configure_logging(SETTINGS["logging_location"])
 
@@ -45,7 +43,11 @@ def main(site_preferences: dict, driver=None, *args, **kwargs):
     if site_preferences.get("login_required"):
         wordhero.login_to_wordhero(SETTINGS["wordhero_credentials"]["email"], SETTINGS["wordhero_credentials"]["password"])
 
-    wordhero.generate_article(**site_preferences["options"])
+    logging.info("Going to generate article...")
+    generated_article, prompt_response_mapping = wordhero.generate_article(**site_preferences["options"])
+    logging.info("Article generated successfully...")
+    WordHero.save_content(generated_article, SETTINGS["output_location"])
+    logging.info("Article saved successfully...")
 
     # Quitting the driver instance if local_webdriver
     if local_webdriver:
