e0dd2370bebc75cb90305f8d2dc879e16fda6605
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-09 22:48:57 +0530

    Feat: Add feat. to clear the display
    Add a method to clear the display.
    
    Signed-off-by: @surajgirioffl

--- cli.py
+++ cli.py
@@ -7,6 +7,17 @@ Last-modified: 09th June 2024
 Error-series: 2200
 """
 
+import importlib
+import sys
+from typing import Literal
+from os import system
 
-def main():
-    pass
+
+def clear_screen():
+    """
+    A function to clear the screen based on the operating system.
+    """
+    if sys.platform == "win32":
+        system("cls")
+    else:
+        system("clear")
