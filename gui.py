83335b23c5c6129b6fd6fd8a4d2b6fc92db0df2c
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-10 10:57:19 +0530

    Add: Add AIGenerator class for the GUI
    Add the AIGenerator class for the GUI.
    
    Signed-off-by: @surajgirioffl

--- gui.py
+++ gui.py
@@ -3,10 +3,16 @@
 GUI module to provide Graphical User Interface for the application.
 Author: Suraj Kumar Giri (@surajgirioffl)
 Init-date: 09th June 2024
-Last-modified: 09th June 2024
+Last-modified: 10th June 2024
 Error-series: 2300
 """
 
+import toga
 
-def main():
+__author__ = "Suraj Kumar Giri"
+__version__ = "0.0.0"
+__description__ = "A browser automation project that uses various AI generation sites to generate videos, images, and text with various options and customizations."
+
+
+class AIGenerator(toga.App):
     pass
