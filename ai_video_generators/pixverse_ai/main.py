0bf5fa0a86bedd7ad99fc83404fe3fdc74c4bdde
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-11 11:15:33 +0530

    Implement: Implement functionality of conditional import
    Implement conditional import to avoid ImportError while executing the
    current module or importing the current module in other module of any
    package.
    
    Signed-off-by: @surajgirioffl

--- ai_video_generators/pixverse_ai/main.py
+++ ai_video_generators/pixverse_ai/main.py
@@ -23,7 +23,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
-from . import pixverse
+
+if __name__ == "__main__":
+    import pixverse
+else:
+    from . import pixverse
 
 PROJECT_DIR = os.path.dirname(__file__)
 os.chdir(PROJECT_DIR)  # Changing the current working directory to the project directory.
