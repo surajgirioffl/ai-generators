b3061be33009d85d6d7bf68f29f95b80afbfb5c5
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-16 03:20:23 +0530

    Add: Add model for the `sites` table
    Add model for the 'sites' table.
    
    Signed-off-by: @surajgirioffl

--- models.py
+++ models.py
@@ -5,3 +5,16 @@ Init-date: 16th June 2024
 Last-modified: 16th June 2024
 Error-series: 2400
 """
+
+from sqlalchemy import Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Sites(Base):
+    __tablename__ = "sites"
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    site_name = mapped_column(String, unique=True, nullable=False)
