9e3ecef8405ce57efdda46aec78284c6a5e33b0a
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-06-17 01:06:31 +0530

    Add: Add a module to perform database operations
    Add a module to perform database operations associated with the
    application.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts.py
@@ -0,0 +1,7 @@
+"""Module to perform database operations associated with the application.
+
+You can import this module to perform database operations for any site.
+Author: Suraj Kumar Giri (@surajgirioffl)
+Init-date: 17th June 2024
+Last-modified: 17th June 2024
+"""
